df55e51dc66c31cac57328fa2dc14bb3d3d21906
DESKTOP-82NRFN8\User <Dkproh@gmail.com>
2021-11-20 02:35:11 +0900

    모든 DB 요청에 모든 인터락을 넣을지 고민 중.

--- src/main/java/org/edwith/webbe/guestbook/dao/GuestbookDao.java
+++ src/main/java/org/edwith/webbe/guestbook/dao/GuestbookDao.java
@@ -6,15 +6,22 @@ import org.edwith.webbe.guestbook.util.DBUtil;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class GuestbookDao {
+	
+	public GuestbookDao () {
+		init();
+	}
+	
     public List<Guestbook> getGuestbooks(){
         List<Guestbook> list = new ArrayList<>();
 
-        // 코드를 작성하세요.
+    	DBUtil dbUnit = new DBUtil();
+    	dbUnit.getTableNames();
 
         return list;
     }
@@ -22,4 +29,11 @@ public class GuestbookDao {
     public void addGuestbook(Guestbook guestbook){
         // 코드를 작성하세요.
     }
+    
+    public boolean init() {
+    	
+    	return true;
+    }
+
+    
 }
