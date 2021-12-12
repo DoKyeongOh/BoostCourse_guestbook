92039f8c224d19573876e5eb9a694b5a8765a40f
DESKTOP-82NRFN8\User <Dkproh@gmail.com>
2021-12-12 14:33:16 +0900

    ddl 정의 부분에 name 컬럼이 빠져있어 추가

--- ddl.sql
+++ ddl.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `guestbook` (
   `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'guestbook id',
+  `name` TEXT NOT NULL COMMENT 'guestbook name',
   `content` TEXT NOT NULL COMMENT 'guestbook content',
   `regdate` DATETIME NULL DEFAULT NULL COMMENT '등록일',
   PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
