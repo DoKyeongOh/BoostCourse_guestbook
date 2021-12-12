43b45e72fb2aee48425720a35dee550bcbf85504
DESKTOP-82NRFN8\User <Dkproh@gmail.com>
2021-12-12 14:45:00 +0900

    DAO에서 DBUtil을 사용하여 해당 테이블에 있는 데이터를 가져오기 위한 과정을 먼저 주석으로 써놓고, 주석을 보며 해당 함수의 의의에서 벗어나지 않으려고 함. 내용은 테이블 없으면 테이블 생성하는 코드

--- src/main/java/org/edwith/webbe/guestbook/dao/GuestbookDao.java
+++ src/main/java/org/edwith/webbe/guestbook/dao/GuestbookDao.java
@@ -14,25 +14,47 @@ import java.util.List;
 public class GuestbookDao {
 	
 	public GuestbookDao () {
-		init();
+		//
 	}
 	
     public List<Guestbook> getGuestbooks(){
+        
+        // 리스트를 불러오기 위한 조건
+        // 1. DB에 관련 테이블 있는지 체크
+        // 2. 관련 테이블 생성
+        // 3. 쿼리문 실행 및 결과 불러오기
+        // 4. 리스트 형태로 담기
+    	
         List<Guestbook> list = new ArrayList<>();
-
-    	DBUtil dbUnit = new DBUtil();
-    	dbUnit.getTableNames();
+        DBUtil dbutil = new DBUtil();
+        String sql;
+        
+        if (!dbutil.findTable("guestbook")) {
+        	sql = "CREATE TABLE `guestbook` ("
+        			  + "`id` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'guestbook id',"
+        			  + "`name` TEXT NOT NULL COMMENT 'guestbook name',"
+        			  + "`content` TEXT NOT NULL COMMENT 'guestbook content',"
+        			  + "`regdate` DATETIME NULL DEFAULT NULL COMMENT '등록일',"
+        			  + "PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+        	if (!dbutil.exec(sql)) return null;
+        }
+        
+        sql = "Select * from guestbook";
+        ResultSet rs = dbutil.execQuery(sql);
+        try {
+			while (rs.next()) {
+				list.add(new Guestbook(rs.getLong(1), rs.getString(2), rs.getString(3), rs.getDate(4)));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
 
         return list;
     }
 
     public void addGuestbook(Guestbook guestbook){
         // 코드를 작성하세요.
-    }
-    
-    public boolean init() {
-    	
-    	return true;
     }
 
     
