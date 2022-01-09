a6ff44499a3eb07ef17ea8d6add0a1b90edd5209
DESKTOP-82NRFN8\User <Dkproh@gmail.com>
2022-01-09 17:29:01 +0900

    regdate를 java.util.date() 형태에서 string형태로 변경

--- src/main/java/org/edwith/webbe/guestbook/dto/Guestbook.java
+++ src/main/java/org/edwith/webbe/guestbook/dto/Guestbook.java
@@ -6,9 +6,9 @@ public class Guestbook {
     private Long id;
     private String name;
     private String content;
-    private Date regdate;
+    private String regdate;
 
-    public Guestbook(Long id, String name, String content, Date regdate) {
+    public Guestbook(Long id, String name, String content, String regdate) {
         this.id = id;
         this.name = name;
         this.content = content;
@@ -18,7 +18,7 @@ public class Guestbook {
     public Guestbook(String name, String content) {
         this.name = name;
         this.content = content;
-        this.regdate = new Date();
+        this.regdate = new Date().toString();
     }
 
     public Long getId() {
@@ -45,11 +45,11 @@ public class Guestbook {
         this.content = content;
     }
 
-    public Date getRegdate() {
+    public String getRegdate() {
         return regdate;
     }
 
-    public void setRegdate(Date regdate) {
+    public void setRegdate(String regdate) {
         this.regdate = regdate;
     }
 
