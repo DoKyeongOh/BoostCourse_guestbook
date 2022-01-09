8e80a9db6b91f96b7e84cd856457c8405afc5aaf
DESKTOP-82NRFN8\User <Dkproh@gmail.com>
2022-01-09 17:28:32 +0900

    기존 findTable 함수같이 서비스 관련 기능을 가진 함수는 dao에 작성

--- src/main/java/org/edwith/webbe/guestbook/dao/GuestbookDao.java
+++ src/main/java/org/edwith/webbe/guestbook/dao/GuestbookDao.java
@@ -2,60 +2,88 @@ package org.edwith.webbe.guestbook.dao;
 
 import org.edwith.webbe.guestbook.dto.Guestbook;
 import org.edwith.webbe.guestbook.util.DBUtil;
+import org.edwith.webbe.guestbook.util.DBUtilFactory;
+import org.edwith.webbe.guestbook.util.DBUtilFactory.DBType;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class GuestbookDao {
+	DBUtilFactory dbFactory = null;
+	DBUtil dbutil = null;
+	List<Guestbook> guestbooks = null;
+	int contentCount = 1;
 	
-	public GuestbookDao () {
-		//
+	public GuestbookDao() {
+		dbFactory = new DBUtilFactory();
+		dbutil = dbFactory.getInstance(DBType.MySQL);
+			
 	}
 	
     public List<Guestbook> getGuestbooks(){
+        List<Guestbook> list = new ArrayList<Guestbook>();
+        createGuestbookTable();
         
-        // 리스트를 불러오기 위한 조건
-        // 1. DB에 관련 테이블 있는지 체크
-        // 2. 관련 테이블 생성
-        // 3. 쿼리문 실행 및 결과 불러오기
-        // 4. 리스트 형태로 담기
-    	
-        List<Guestbook> list = new ArrayList<>();
-        DBUtil dbutil = new DBUtil();
         String sql;
-        
-        if (!dbutil.findTable("guestbook")) {
-        	sql = "CREATE TABLE `guestbook` ("
-        			  + "`id` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'guestbook id',"
-        			  + "`name` TEXT NOT NULL COMMENT 'guestbook name',"
-        			  + "`content` TEXT NOT NULL COMMENT 'guestbook content',"
-        			  + "`regdate` DATETIME NULL DEFAULT NULL COMMENT '등록일',"
-        			  + "PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
-        	if (!dbutil.exec(sql)) return null;
-        }
-        
-        sql = "Select * from guestbook";
+        sql = "Select id, name, content, regdate from guestbook";
         ResultSet rs = dbutil.execQuery(sql);
         try {
 			while (rs.next()) {
-				list.add(new Guestbook(rs.getLong(1), rs.getString(2), rs.getString(3), rs.getDate(4)));
+				list.add(new Guestbook(rs.getLong(1), rs.getString(2), rs.getString(3), rs.getString(4)));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
 		}
-
+        
+        guestbooks = list;
         return list;
     }
 
     public void addGuestbook(Guestbook guestbook){
-        // 코드를 작성하세요.
+    	createGuestbookTable();
+    	String sql = "insert into guestbook values(" 
+    						+ Long.toString(guestbook.getId()) + ",\""
+    						+ guestbook.getName() + "\",\""
+    						+ guestbook.getContent() + "\","
+    						+ guestbook.getRegdate() + ");";
+    	dbutil.execUpdate(sql);
     }
-
+	
+	public boolean findTable(String tablename) {
+		List<String> list = new ArrayList<String>();
+		String sql = "show tables";
+		
+		ResultSet rs = dbutil.execQuery(sql);
+		if (rs == null) return false;
+		
+		
+		try {
+			while(rs.next()) {
+				// 테이블 컬럼의 인덱스는 1부터인가보다
+				list.add(rs.getString(1));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		if (list.contains(tablename)) return true;
+		else return false;
+	}
+    
+	public boolean createGuestbookTable() {
+        if (!findTable("guestbook")) {
+        	String sql = "CREATE TABLE `guestbook` ("
+        			  + "`id` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'guestbook id',"
+        			  + "`name` TEXT NOT NULL COMMENT 'guestbook name',"
+        			  + "`content` TEXT NOT NULL COMMENT 'guestbook content',"
+        			  + "`regdate` TEXT NULL DEFAULT NULL COMMENT '등록일',"
+        			  + "PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+        	if (!dbutil.execUpdate(sql)) return false;
+        }
+        return true;
+	}
     
 }
